eea2078a1be702d95303c8c4047629bfe5e68837
wkddmstn <wkddmstn3020@gmail.com>
2025-06-24 17:06:34 +0900

    commit7

--- src/main/java/org/example/cicd_test3/controller/MainController.java
+++ src/main/java/org/example/cicd_test3/controller/MainController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class MainController {
     @GetMapping
     private String hello() {
-        return "This is auto deployment with container4";
+        return "This is auto deployment with container5";
     }
 }
